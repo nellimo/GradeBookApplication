23d212eeb20e5b741167518626af509d8da3a1d4
nellimo <nelli.motieva@microsoft.wsei.edu.pl>
2026-04-12 22:24:52 +0200

    Make the BaseGradeBook class abstract

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -44,7 +44,7 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             var type = parts[2];
 
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
 
             if (type == "standard")
             {
